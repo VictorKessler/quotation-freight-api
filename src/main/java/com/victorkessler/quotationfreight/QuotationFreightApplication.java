ecd85b0877d1e1c5bcae1ec7ab678da70986785e
Victor Kessler Soares Pereira <victorkessler26@gmail.com>
2023-05-05 11:53:57 -0300

    Initial commit

---
+++ src/main/java/com/victorkessler/quotationfreight/QuotationFreightApplication.java
@@ -0,0 +1,13 @@
+package com.victorkessler.quotationfreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationFreightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationFreightApplication.class, args);
+	}
+
+}
